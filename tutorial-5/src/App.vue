4d33b2ad13e3a67e9afd928cf3f67ebc3a8bce4e
Polok Banik <polokbanik7@gmail.com>
2025-08-01 17:26:17 +0600

    add tutorial-5

---
+++ tutorial-5/src/App.vue
@@ -0,0 +1,41 @@
+<script>
+import Header from "../src/components/Header.vue";
+export default {
+  //   data() {
+  //     return {
+  //       awesome: false,
+  //     };
+  //   },
+  //   methods: {
+  //     toggle(value) {
+  //       this.awesome = value;
+  //     },
+  //   },
+
+  components: {
+    Header,
+  },
+};
+</script>
+
+<template>
+  <Header />
+  <!-- <div v-if="awesome">
+    <h1>P</h1>
+    <h1>o</h1>
+    <h1>l</h1>
+    <h1>o</h1>
+    <h1>k</h1>
+  </div> -->
+
+  <!-- <div>
+    <div>
+      <h1 v-if="awesome === 'Polok'">Polok is Awesome!</h1>
+      <h1 v-else-if="awesome === 'Banik'">Banik is Awesome!</h1>
+      <h1 v-else>Vue is Super!</h1>
+    </div>
+    <button @click="toggle">Toggle</button>
+  </div> -->
+</template>
+
+<style scoped></style>
---
+++ tutorial-5/src/components/Header.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      isUserLoggedIn: false,
+    };
+  },
+
+  methods: {
+    signIn() {
+      this.isUserLoggedIn = true;
+    },
+    logout() {
+      this.isUserLoggedIn = false;
+    },
+  },
+};
+</script>
+
+<template>
+  <header>
+    <h1>POLOK</h1>
+
+    <div v-if="!isUserLoggedIn">
+      <button @click="signIn">SIGN IN</button>
+      <button>SIGN UP</button>
+    </div>
+
+    <div v-else>
+      <button>PROFILE</button>
+      <button @click="logout">LOG OUT</button>
+    </div>
+  </header>
+</template>
+
+<style scoped></style>
